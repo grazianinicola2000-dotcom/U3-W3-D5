7528d60cd903d9ade187c0e97364f9b2052821fb
Graziani Nicola <graziani.nicola2000@gmail.com>
2026-02-20 16:57:54 +0100

    .

--- src/components/MoreToExplore.jsx
+++ src/components/MoreToExplore.jsx
@@ -4,7 +4,7 @@ const MoreToExplore = function () {
   return (
     <section className="pr-[30px]">
       <h2 className="mb-2 text-xl mt-8">Altro da esplorare</h2>
-      <div className="grid grid-cols-1 auto-rows-[60px] text-info">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-x-2 auto-rows-[60px] text-info">
         <div className="bg-bg-secondary row-span-1 col-span-1 flex items-center px-3 rounded-lg flex justify-between mb-2">
           <p className="text-sm">Esplora per genere</p>
           <ChevronRight className="w-[20px]" />
