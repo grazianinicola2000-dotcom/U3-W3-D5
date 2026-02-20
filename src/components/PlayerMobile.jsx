a61795eeb6d69981f78ac6f4debad76ce57c5893
Graziani Nicola <graziani.nicola2000@gmail.com>
2026-02-20 16:56:06 +0100

    fix

--- src/components/PlayerMobile.jsx
+++ src/components/PlayerMobile.jsx
@@ -8,12 +8,14 @@ const PlayerMobile = function () {
   return (
     <section className="md:hidden flex justify-center">
       <div className="w-[90%] flex items-center justify-between px-3 bg-bg-secondary py-2 rounded-lg mx-3 fixed bottom-0 mb-3 shadow-xl">
-        <div className="w-[50px]">
-          <img className="rounded-md" src={currentSong.album.cover} alt="Song_cover" />
-        </div>
-        <div className="">
-          <p>{currentSong.title_short}</p>
-          <p>{currentSong.artist.name}</p>
+        <div className="flex items-center">
+          <div className="w-[50px]">
+            <img className="rounded-md" src={currentSong.album.cover} alt="Song_cover" />
+          </div>
+          <div className="ml-2">
+            <p className="text-md">{currentSong.title_short}</p>
+            <p className="text-sm text-icons">{currentSong.artist.name}</p>
+          </div>
         </div>
         <div className="text-text-footer flex gap-5">
           <i className="bi bi-play-fill text-3xl"></i>
